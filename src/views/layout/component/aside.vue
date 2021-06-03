606dcb7ce87df83c3382e5b1a9ad2917aa431862
xiaoran <303176152@qq.com>
2021-06-03 17:18:03 +0800

    搭建

--- src/views/layout/component/aside.vue
+++ src/views/layout/component/aside.vue
@@ -1,12 +1,10 @@
 <template>
-    <Defaults />
+    <el-aside class="layout-aside"> </el-aside>
 </template>
 <script lang="ts">
 import { defineComponent } from 'vue';
-import Defaults from '/@/views/layout/main/defaults.vue';
 export default defineComponent({
-    name: 'Layout',
-    components: { Defaults },
+    name: 'LayoutAside',
 });
 </script>
 <style lang="scss"></style>
---
+++ src/views/layout/main/defaults.vue
@@ -0,0 +1,14 @@
+<template>
+    <el-container class="layout-container">
+        <Aside />
+    </el-container>
+</template>
+<script lang="ts">
+import { defineComponent } from 'vue';
+import Aside from '../component/aside.vue';
+export default defineComponent({
+    name: 'LayoutDefaults',
+    components: { Aside },
+});
+</script>
+<style lang="scss"></style>
